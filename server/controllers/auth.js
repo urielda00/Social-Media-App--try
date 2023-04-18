0977af3ef0732a6235c022cef27e37728e83a157
urielda00 <urielda00@gmail.com>
2023-04-18 15:39:43 +0300

    complited the user register in the auth controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,45 @@
 import bcrypt from 'bcrypt';
-import { Jwt } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
+
+
+//register user:
+
+export const register= async (req, res)=>{
+  try {
+    const {
+      firstName,
+      lastName, 
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation
+    }= req.body;
+
+  const salt = await bcrypt.genSalt();
+  const passwordHash= await bcrypt.hash(password, salt)
+
+  const newUser= new User({
+    firstName,
+    lastName, 
+    email,
+    password: passwordHash,
+    picturePath,
+    friends,
+    location,
+    occupation,
+    //for now- some random calc for this two: 
+    viewedProfile: Math.floor(Math.random() *10000),
+    impressions: Math.floor(Math.random() *10000)
+  });
+
+  //now saving : 
+  const savedUser= await newUser.save();
+  res.status(201).json(savedUser);
+
+  } catch (err) {
+    res.status(500).json({error: err.message})
+  }
+}
