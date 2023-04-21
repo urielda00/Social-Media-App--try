c7fc72fa7948e078231c35e00d7c8b37bee90290
urielda00 <urielda00@gmail.com>
2023-04-21 14:20:38 +0300

    added setup for the persist condig, and bug fixing

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+
+
 function App() {
-  return 
-    <div className="app">
-      <BrowserRouter>
+  const mode= useSelector((state)=>state.mode); //return the mode- dark/light.
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return (
+  <div className="app">
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Routes>
         <Route path="/" element={<LoginPage/>}/>
         <Route path="/home" element={<HomePage/>}/>
         <Route path="/profile/:userId" element={<ProfilePage/>}/> 
       </Routes>
+      </ThemeProvider> 
       </BrowserRouter>
-    </div>;
+    </div>);
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,38 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import authReducer from './state';
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import { Provider } from 'react-redux';
-
-//only import when usin redux persist:
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import authReducer from "./state";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 import {
- persistStore,
- persistReducer,
- FLUSH,
- REHYDRATE,
- PAUSE,
- PERSIST,
- PURGE,
- REGISTER
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { PersistGate } from 'redux-persist/integration/react';
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 
-const persistConfig= { key: 'root', storage, version:1  };
-const persistedReducer= persistReducer(persistConfig, authReducer);
-const store= configureStore({
-  reducer:persistedReducer,
-  middleware:(getDefaultMiddleware)=>
-  getDefaultMiddleware({
-    serializableCheck:{
-      ignoreActions: [FLUSH, REHYDRATE, PAUSE,PERSIST,PURGE,REGISTER],
-    }
-  })
-})
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -42,5 +40,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
- 
